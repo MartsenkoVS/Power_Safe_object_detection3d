9d7f8832f55b2136c211b590b8a5a5e463f93a0e
Martsenko Vitalii <162605977+MartsenkoVS@users.noreply.github.com>
2024-11-23 12:01:52 +0000

    Update my_dataset.py

--- my_dataset.py
+++ my_dataset.py
@@ -1,7 +1,8 @@
-import numpy as np
 from mmdet3d.registry import DATASETS
 from mmdet3d.datasets import Det3DDataset
 from mmdet3d.structures import LiDARInstance3DBoxes
+import numpy as np
+import mmengine
 
 @DATASETS.register_module()
 class MyDataset(Det3DDataset):
@@ -10,18 +11,25 @@ class MyDataset(Det3DDataset):
         'classes': ('LEP_metal', 'LEP_prom', 'vegetation')
     }
 
+    def load_data_list(self):
+        """Загружает список данных из файла аннотаций."""
+        if isinstance(self.ann_file, str):
+            data_list = mmengine.load(self.ann_file)
+        else:
+            data_list = self.ann_file
+        return data_list
+
     def parse_ann_info(self, info):
-        """Process the `instances` in data info to `ann_info`."""
+        """Обрабатывает `instances` в `info` и возвращает `ann_info`."""
         ann_info = super().parse_ann_info(info)
         if ann_info is None:
             ann_info = dict()
-            # empty instance
+            # Пустой экземпляр
             ann_info['gt_bboxes_3d'] = np.zeros((0, 7), dtype=np.float32)
             ann_info['gt_labels_3d'] = np.zeros(0, dtype=np.int64)
 
-        # Filter classes not used in training
+        # Фильтруем классы, не используемые в обучении
         ann_info = self._remove_dontcare(ann_info)
         gt_bboxes_3d = LiDARInstance3DBoxes(ann_info['gt_bboxes_3d'])
         ann_info['gt_bboxes_3d'] = gt_bboxes_3d
         return ann_info
-
