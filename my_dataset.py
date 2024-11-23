cffbdb56ed3cdc5f90fc1733ffb50b5f1618a522
Martsenko Vitalii <162605977+MartsenkoVS@users.noreply.github.com>
2024-11-23 12:34:11 +0000

    Update my_dataset.py

--- my_dataset.py
+++ my_dataset.py
@@ -1,35 +1,64 @@
 from mmdet3d.registry import DATASETS
-from mmdet3d.datasets import Det3DDataset
+from mmdet3d.datasets import Custom3DDataset
 from mmdet3d.structures import LiDARInstance3DBoxes
 import numpy as np
-import mmengine
+import os.path as osp
 
 @DATASETS.register_module()
-class MyDataset(Det3DDataset):
+class MyDataset(Custom3DDataset):
 
     METAINFO = {
         'classes': ('LEP_metal', 'LEP_prom', 'vegetation')
     }
 
+    def __init__(self,
+                 data_root,
+                 ann_file,
+                 pipeline=None,
+                 classes=None,
+                 modality=None,
+                 filter_empty_gt=True,
+                 test_mode=False):
+        super().__init__(
+            data_root=data_root,
+            ann_file=ann_file,
+            pipeline=pipeline,
+            classes=classes,
+            modality=modality,
+            filter_empty_gt=filter_empty_gt,
+            test_mode=test_mode)
+        self.data_root = data_root
+
     def load_data_list(self):
         """Загружает список данных из файла аннотаций."""
-        if isinstance(self.ann_file, str):
-            data_list = mmengine.load(self.ann_file)
-        else:
-            data_list = self.ann_file
+        data_list = mmengine.load(osp.join(self.data_root, self.ann_file))
         return data_list
 
-    def parse_ann_info(self, info):
-        """Обрабатывает `instances` в `info` и возвращает `ann_info`."""
-        ann_info = super().parse_ann_info(info)
-        if ann_info is None:
-            ann_info = dict()
-            # Пустой экземпляр
-            ann_info['gt_bboxes_3d'] = np.zeros((0, 7), dtype=np.float32)
-            ann_info['gt_labels_3d'] = np.zeros(0, dtype=np.int64)
+    def get_data_info(self, index):
+        info = self.data_list[index]
+        input_dict = dict(
+            sample_idx=index,
+            pts_filename=osp.join(self.data_root, info['point_cloud']['point_cloud_path']),
+            timestamp=info.get('timestamp', 0),
+        )
+        return input_dict
 
-        # Фильтруем классы, не используемые в обучении
-        ann_info = self._remove_dontcare(ann_info)
-        gt_bboxes_3d = LiDARInstance3DBoxes(ann_info['gt_bboxes_3d'])
-        ann_info['gt_bboxes_3d'] = gt_bboxes_3d
-        return ann_info
+    def get_ann_info(self, index):
+        info = self.data_list[index]
+        annos = info.get('annos', None)
+        if annos is None or len(annos['name']) == 0:
+            gt_bboxes_3d = np.zeros((0, 7), dtype=np.float32)
+            gt_labels_3d = np.zeros((0, ), dtype=np.int64)
+        else:
+            names = annos['name']
+            dims = np.array(annos['dimensions'])
+            locs = np.array(annos['location'])
+            rots = np.array(annos['rotation_y'])
+            gt_bboxes_3d = np.concatenate([locs, dims, rots[:, np.newaxis]], axis=1)
+            gt_labels_3d = np.array([self.metainfo['classes'].index(n) for n in names], dtype=np.int64)
+        gt_bboxes_3d = LiDARInstance3DBoxes(gt_bboxes_3d)
+        anns_results = dict(
+            gt_bboxes_3d=gt_bboxes_3d,
+            gt_labels_3d=gt_labels_3d,
+        )
+        return anns_results
